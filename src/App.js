6b4ca8ce74b0ce4cb0353e0b480cc2d4b75d4935
Jan_Lebar <janstefanlebar@gmail.com>
2023-05-26 18:30:50 +0200

    Layout and using {props.children} to wrap around modules

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
+
+
+
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import AllMeetupsPage from './pages/AllMeetups';
 import NewMeetupPage from './pages/NewMeetups';
 import FavoritesPage from './pages/Favorites';
-import MainNavigation from "./components/layout/MainNavigation";
+import Layout from './components/layout/Layout';
 
-
-  function App() {
-    return (
-      
+function App() {
+  return (
+    
       <BrowserRouter>
-      <MainNavigation />
-      <Routes>
-        <Route path='/' exact element={<AllMeetupsPage/>} />
-       <Route path='/new-meetup' element={<NewMeetupPage/>} />
-        <Route path='/favorites' element={<FavoritesPage/>}/>
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path='/' element={<AllMeetupsPage />} />
+          <Route path='/new-meetup' element={<NewMeetupPage />} />
+          <Route path='/favorites' element={<FavoritesPage />} />
+        </Routes>
+        </Layout>
       </BrowserRouter>
-    );
-  }
-  
+
+  );
+}
 
 export default App;
