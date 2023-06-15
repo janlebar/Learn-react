d0c9830daddd20401e332d7484bdfd54921cf0e8
Jan_Lebar <janstefanlebar@gmail.com>
2023-06-15 15:06:49 +0200

    AllMeetups dummy data removed and now uses FIREBASE database. Looping through the OBJECT           const meetup = {             id: key,             ...data[key]           }; because you get from database an obect not a list which you can map through! tutorial 27-29

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,35 +1,71 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import MeetupList from '../components/meetups/MeetupsList';
 
-const DUMMY_DATA = [
-  {
-    id: 'm1',
-    title: 'This is a first meetup',
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
-    address: 'Meetupstreet 5, 12345 Meetup City',
-    description:
-      'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
-  },
-  {
-    id: 'm2',
-    title: 'This is a second meetup',
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
-    address: 'Meetupstreet 5, 12345 Meetup City',
-    description:
-      'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
-  },
-];
-
+// Import necessary modules and components
 
 function AllMeetupsPage() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedMeetups, setLoadedMeetups] = useState([]);
+
+  // Define states 'isLoading' and 'loadedMeetups' using the 'useState' hook
+
+  useEffect(() => {
+    setIsLoading(true);
+    // Set 'isLoading' to 'true'
+
+    fetch(
+      'https://test-react-59f5a-default-rtdb.europe-west1.firebasedatabase.app/meetups.json'
+    )
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        const meetups = [];
+
+        for (const key in data) {
+          // Iterate over each key in the 'data' object
+
+          const meetup = {
+            id: key,
+            ...data[key]
+          };
+          // Create a 'meetup' object with an 'id' field equal to the current key
+          // and all other properties found in 'data[key]'
+
+          meetups.push(meetup);
+          // Add the 'meetup' object to the 'meetups' array using the 'push' method
+          // (note: there was a mistake in adding to the array, it should be 'meetups.push(meetup);')
+        }
+        // Extract data from the response and form 'meetup' objects
+
+        setIsLoading(false);
+        // Set 'isLoading' to 'false'
+
+        setLoadedMeetups(meetups);
+        // Set the 'loadedMeetups' state to the 'meetups' array
+      });
+  }, []);
+
+  // Perform the asynchronous effect after component mounting and when an empty array [] changes
+
+  if (isLoading) {
+    return (
+      <section>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
+  // If data is still loading, display the text "Loading..."
+
   return (
     <section>
-      <h1>AllMeetups</h1>
-      <MeetupList meetups={DUMMY_DATA}/>
+      <h1>All Meetups</h1>
+      <MeetupList meetups={loadedMeetups} />
     </section>
   );
 }
+
+// Display a list of meetups once the data is loaded
 
 export default AllMeetupsPage;
